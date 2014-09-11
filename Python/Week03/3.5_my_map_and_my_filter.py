076a766388cd370779dfbb3b308c4034aa1aaa1c
Igor Leonenko <moow.moow@yandex.ru>
2014-09-12 00:34:22 +0400

    задание 3.5

--- Python/Week03/3.5_my_map_and_my_filter.py
+++ Python/Week03/3.5_my_map_and_my_filter.py
@@ -3,12 +3,22 @@
 def my_map(func_name, iterable_obj):
     res = []
     
-    [res.append(func_name(x)) for x in l]
+    [res.append(func_name(x)) for x in iterable_obj]
     
     return res
 
 def my_filter(func_name, iterable_obj):
-    pass
+    res = []
+    
+    for x in iterable_obj:
+        if func_name != None:
+            if func_name(x):
+                res.append(x)
+        else:
+            if not x:
+                res.append(x)
+            
+    return res
     
 
 if __name__ == "__main__":
