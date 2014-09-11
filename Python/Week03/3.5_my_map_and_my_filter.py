415e6f6229cb90eb92a34c69cf9c9e7998ccb8f9
LeoIgor <moow.moow@yandex.ru>
2014-09-11 20:32:16 +0400

    new file

--- Python/Week03/3.5_my_map_and_my_filter.py
+++ Python/Week03/3.5_my_map_and_my_filter.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
-def my_map():
+def my_map(func_name, iterable_obj):
+    res = []
+    
+    [res.append(func_name(x)) for x in l]
+    
+    return res
+
+def my_filter(func_name, iterable_obj):
     pass
     
-def my_filter():
-    pass
-
 
 if __name__ == "__main__":
-    my_map()
+    pass
